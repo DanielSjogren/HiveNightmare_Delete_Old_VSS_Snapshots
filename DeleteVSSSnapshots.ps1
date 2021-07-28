06afee97d54aa642b1e87ad9e967512f51404204
Daniel Sjögren <Daniel.Sjogren@permobil.com>
2021-07-28 10:07:32 +0200

    Update DeleteVSSSnapshots.ps1
    Polished the regex pattern, added output for amount of Shadow copies, added part to lookup Users group name

--- DeleteVSSSnapshots.ps1
+++ DeleteVSSSnapshots.ps1
@@ -1,7 +1,7 @@
 # Create regex pattern for VSSadmin command
-$pattern = 'Contained 1 shadow copies at creation time: (\S*\d*\s\S*\d*)[\s\S]*?' +
-'Shadow Copy ID: ([{]\w*\d*[-]\w*\d*[-]\w*\d*[-]\w*\d*[-]\w*\d*[}])[\s\S]*?' +
-'Original Volume: \S*[\s\S]*?' +
+$pattern = 'Contained 1 shadow copies at creation time: (.*)[\s\S]*?' +
+'Shadow Copy ID: ([{](.*)[}])[\s\S]*?' +
+'Original Volume: .*[\s\S]*?' +
 'Shadow Copy Volume: ([\\][\\]\S*)'
 
 # Create a variable for available Shadow Copies
@@ -17,11 +17,16 @@ $ShadowCopies = & vssadmin list shadows /for=C: | Out-String |
     } 
   }
 
+  Write-Output "Total Copies: $($ShadowCopies.Count)"  
+
+  # Get Local Users groupname
+  $LocalBuiltInUsersGroupName = (Get-Localgroup -SID S-1-5-32-545).Name
+
 # Mount each Shadow Copy and verify if Users have access to SAM database. If yes, then delete the snapshot
 ForEach ($ShadowCopy in $ShadowCopies) {
-    "ShadowCopy Creation Time: $($ShadowCopy.CreationTime)"
-    "ShadowCopy ID: $($ShadowCopy.ShadowCopyID)"
-    "ShadowCopy Path: $($ShadowCopy.ShadowCopyPath)"
+    Write-Output "ShadowCopy Creation Time: $($ShadowCopy.CreationTime)"
+    Write-Output "ShadowCopy ID: $($ShadowCopy.ShadowCopyID)"
+    Write-Output "ShadowCopy Path: $($ShadowCopy.ShadowCopyPath)"
 
     $ShadowPath = $ShadowCopy.ShadowCopyPath
     $SnapShotId = $ShadowCopy.ShadowCopyID
@@ -38,7 +43,7 @@ ForEach ($ShadowCopy in $ShadowCopies) {
         $SAMPath = "$shadowMount\Windows\System32\Config\SAM"
         If (Test-Path $SAMPath) {   
             Write-Output "-- SAM Database found, checking permissions"
-            If ((Get-Acl $SAMPath).access.IdentityReference -contains "BUILTIN\Users") {
+            If ((Get-Acl $SAMPath).access.IdentityReference -contains "BUILTIN\$LocalBuiltInUsersGroupName") {
                 Write-Output "--- Users can read this file, deleting Shadow Copy"
                 $newSession=New-PSSession
                 Invoke-Command -Session $newSession -ScriptBlock{param($SnapShotId);vssadmin delete shadows /Shadow=$SnapShotId /quiet} -args $SnapShotId
